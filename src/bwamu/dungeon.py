23cb5ba19c0ddcea555fd84a02f9017ee740d554
Noved <morph8hprom@gmail.com>
2018-05-28 12:32:46 -0500

    Finished dungeon methods.  All tests working properly

--- src/bwamu/dungeon.py
+++ src/bwamu/dungeon.py
@@ -5,7 +5,11 @@ from bwamu import dungeon_map as dun_map
 class Dungeon():
     def __init__(self, id = '1', name = 'test name',
                  desc = 'test desc', map = None):
-                 self.id = id
-                 self.name = name
-                 self.desc = desc
-                 self.map = map
+                 self._id = id
+                 self._name = name
+                 self._desc = desc
+                 self._map = map
+
+    def _update_map(self):
+        self._map = dun_map.DungeonMap(self._id)
+        self._map._update_rooms()
--- tests/test_dungeon.py
+++ tests/test_dungeon.py
@@ -17,17 +17,34 @@ class DungeonAttributesTestCase(unittest.TestCase):
         cls.test_dungeon = dungeon.Dungeon()
 
     def test_dungeon_has_id(self):
-        att = hasattr(self.test_dungeon, 'id')
+        att = hasattr(self.test_dungeon, '_id')
         self.assertTrue(att)
 
     def test_dungeon_has_name(self):
-        att = hasattr(self.test_dungeon, 'name')
+        att = hasattr(self.test_dungeon, '_name')
         self.assertTrue(att)
 
     def test_dungeon_has_desc(self):
-        att = hasattr(self.test_dungeon, 'desc')
+        att = hasattr(self.test_dungeon, '_desc')
         self.assertTrue(att)
 
     def test_dungeon_has_map(self):
-        att = hasattr(self.test_dungeon, 'map')
+        att = hasattr(self.test_dungeon, '_map')
         self.assertTrue(att)
+
+class DungeonMethodsTestCase(unittest.TestCase):
+    """
+    Contains tests for Dungeon instance methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Builds test instance of Dungeon
+        """
+        cls.test_dungeon = dungeon.Dungeon()
+
+    def test_dungeon_update_map(self):
+        self.test_dungeon._update_map()
+        att = self.test_dungeon._map
+        self.assertIsNotNone(att)
