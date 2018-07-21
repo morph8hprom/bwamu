f17bb2861fdec76b43b46c4251a6f06e2def9b71
Noved <morph8hprom@gmail.com>
2018-07-21 12:55:15 -0500

    moved shop class to area.py

--- src/bwamu/area.py
+++ src/bwamu/area.py
@@ -4,6 +4,8 @@
 Defines Area class and related methods
 """
 
+from gim import shop_dict as Sd
+
 class Area():
     def __init__(self, id = '1', name = 'test name',
                 desc = 'test desc', area_type = None):
@@ -11,3 +13,16 @@ class Area():
         self._name = name
         self._desc = desc
         self._area_type = area_type
+
+class Shop(area.Area):
+    def __init__(self, id = '1', name = 'test name', desc = 'test desc',
+                area_type = 'Shop', shopkeeper = None,
+                items = [], stock = None):
+        super().__init__(id, name, desc, area_type)
+        self._shopkeeper = shopkeeper
+        self._items = items
+        self._stock = stock
+        self._update_stock()
+
+    def _update_stock(self):
+        self._stock = Sd.ShopDict(items = self._items)
