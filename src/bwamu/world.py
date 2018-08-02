79cf23813eb4feddb732ac37e9173be27596c3c1
Noved <morph8hprom@gmail.com>
2018-08-02 12:50:20 -0500

    Updated world.py removed comments and added attributes to innit

--- src/bwamu/world.py
+++ src/bwamu/world.py
@@ -1,20 +1,16 @@
-# #!/usr/bin/env python3
-#
-# import json
-#
-# """
-# Defines a world class and a functon used to create an instance of the world class
-# """
-#
-# class World():
-#     def __init__(self,
-#         map,
-#         start_loc):
-#         self.map = map
-#         self.start_loc = start_loc
-#
-# def build_world(map, start_loc):
-#     print("Building world")
-#     world = World(map, start_loc)
-#
-#     return world
+#!/usr/bin/env python3
+
+import json
+
+"""
+Defines a world class and a functon used to create an instance of the world class
+"""
+
+class World():
+    def __init__(self,
+        map,
+        player):
+        self.map = map
+        self.player = player
+        # self.start_loc = self.player.loc
+        self.map._update_areas()
