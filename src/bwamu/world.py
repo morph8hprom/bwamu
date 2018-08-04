91f017ee521a7ead46a706766d7f6c1833d38347
Noved <morph8hprom@gmail.com>
2018-08-04 11:03:55 -0500

    Updated attributes for default start location.  Updated docstring

--- src/bwamu/world.py
+++ src/bwamu/world.py
@@ -6,7 +6,7 @@ from upacc import character_dict as cd
 from gim import item_dict as id
 
 """
-Defines a world class and a functon used to create an instance of the world class
+File used to define World class and it's methods
 """
 
 class World():
@@ -14,11 +14,13 @@ class World():
         START_ID = 1,
         NUM_OF_AREAS = 1,
         NUM_OF_ITEMS = 1,
-        NUM_OF_CHARS = 1):
+        NUM_OF_CHARS = 1
+        DEFAULT_START_LOC = 1):
         self.START_ID = START_ID
         self.NUM_OF_AREAS = NUM_OF_AREAS
         self.NUM_OF_ITEMS = NUM_OF_ITEMS
         self.NUM_OF_CHARS = NUM_OF_CHARS
+        self.START_LOC = DEFAULT_START_LOC
 
         # Create instance of Mainmap and update areas
         self.map = mm.Mainmap(self.START_ID, self.NUM_OF_AREAS)
