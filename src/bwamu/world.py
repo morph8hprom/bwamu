d41d03977de54095dffcecdca46b75c30604725b
Noved <morph8hprom@gmail.com>
2018-08-08 21:09:02 -0500

    Fixed broken syntax

--- src/bwamu/world.py
+++ src/bwamu/world.py
@@ -14,7 +14,7 @@ class World():
         START_ID = 1,
         NUM_OF_AREAS = 1,
         NUM_OF_ITEMS = 1,
-        NUM_OF_CHARS = 1
+        NUM_OF_CHARS = 1,
         DEFAULT_START_LOC = 1):
         self.START_ID = START_ID
         self.NUM_OF_AREAS = NUM_OF_AREAS
